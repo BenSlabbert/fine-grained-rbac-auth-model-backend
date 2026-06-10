72992a34a4c4c5d00bbbb02976af9954688a60a3
benjamin.slabbert <benjamin.slabbert@gmail.com>
2026-06-10 19:35:19 +0200

    adding constraint

--- app/src/main/java/org/example/app/web/route/AddMerchantToCustomMerchantGroupRequest.java
+++ app/src/main/java/org/example/app/web/route/AddMerchantToCustomMerchantGroupRequest.java
@@ -4,10 +4,13 @@ package org.example.app.web.route;
 import github.benslabbert.vdw.codegen.annotation.builder.GenerateBuilder;
 import github.benslabbert.vdw.codegen.annotation.json.JsonWriter;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.Set;
 
 @JsonWriter
 @GenerateBuilder
 public record AddMerchantToCustomMerchantGroupRequest(
-    @NotBlank String customMerchantGroupName, @NotNull Set<String> merchantNames) {}
+    @NotBlank String customMerchantGroupName,
+    @NotNull @NotEmpty @Size(min = 1, max = 100) Set<String> merchantNames) {}
