a2ba2e4a89f93a95ca98f7786360da47849545c1
benjamin.slabbert <benjamin.slabbert@gmail.com>
2026-06-20 20:56:25 +0200

    clean up

--- gateway/src/main/java/org/example/gateway/web/route/LogoutHandler.java
+++ gateway/src/main/java/org/example/gateway/web/route/LogoutHandler.java
@@ -5,18 +5,23 @@ import github.benslabbert.vdw.codegen.annotation.auth.NoAuthCheck;
 import github.benslabbert.vdw.codegen.annotation.web.WebHandler;
 import github.benslabbert.vdw.codegen.annotation.web.WebRequest;
 import github.benslabbert.vdw.codegen.annotation.web.WebRequest.Post;
+import io.vertx.ext.auth.User;
 import io.vertx.ext.web.UserContext;
 import jakarta.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @NoAuthCheck
 @WebHandler(path = "/logout")
 class LogoutHandler {
+  private static final Logger log = LoggerFactory.getLogger(LogoutHandler.class);
 
   @Inject
   LogoutHandler() {}
 
   @Post
-  void logout(@WebRequest.UserContext UserContext ctx) {
+  void logout(@WebRequest.User User user, @WebRequest.UserContext UserContext ctx) {
+    log.info("logout {}", user.subject());
     ctx.logout();
   }
 }
--- gateway/src/main/java/org/example/gateway/web/route/Ping.java
+++ gateway/src/main/java/org/example/gateway/web/route/Ping.java
@@ -5,10 +5,7 @@ import github.benslabbert.vdw.codegen.annotation.auth.NoAuthCheck;
 import github.benslabbert.vdw.codegen.annotation.web.WebHandler;
 import github.benslabbert.vdw.codegen.annotation.web.WebRequest;
 import github.benslabbert.vdw.codegen.annotation.web.WebRequest.Get;
-import io.vertx.ext.auth.User;
 import io.vertx.ext.web.RoutingContext;
-import io.vertx.ext.web.Session;
-import io.vertx.ext.web.UserContext;
 import jakarta.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,10 +21,6 @@ class Ping {
 
   @Get
   void ping(@WebRequest.RoutingContext RoutingContext ctx) {
-    // the user is here, it is added by
-    User user = ctx.user();
-    UserContext userContext = ctx.userContext();
-    Session session = ctx.session();
     log.debug("Ping - pong");
     ctx.response().end("pong");
   }
