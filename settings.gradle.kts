b91d11676045e92bc000bdd2be68968fad84d0b4
benjamin.slabbert <benjamin.slabbert@gmail.com>
2026-06-26 22:23:30 +0200

    update build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,11 @@
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("com.gradle.develocity") version ("4.4.3")
 }
 
 rootProject.name = "backend"
 
 include("iam", "utilities", "security-api", "transactions", "gateway")
+
+develocity { buildScan { publishing { onlyIf { false } } } }
